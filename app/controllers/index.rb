fe8c1290546e3685d81659ea81d726bff6210658
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-10 16:57:39 -0500

    mvp achieved

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,28 +1,58 @@
+# using Fake Pat Mood twitter account  username fakepatmood password:passwordpassword
+
+# Access level   Read, write, and direct messages 
+# About the application permission model
+# Consumer key  QpCKEfP0eQImD7GaUdfdw
+# Consumer secret JtxnHqUxZQ6fI4T4HFofZa2X1qZCvB2CHkouVuVMI
+# Request token URL https://api.twitter.com/oauth/request_token
+# Authorize URL https://api.twitter.com/oauth/authorize
+# Access token URL  https://api.twitter.com/oauth/access_token
+
+# Access token  1578293246-UJyL7DftALHb4VAyi5JozxqjMlI6wwtbx7JRBi8
+# Access token secret tOVbjd8VjU77RerEXHFJTiqaG7lE76u4ZGQtwExK7R4
+# Access level  Read, write, and direct messages
+
 get '/' do
   erb :index
 end
 
-get '/:username' do
-  @user = TwitterUser.find_by_handle(params[:username])
-
-  if !@user 
-
-    @user = TwitterUser.create(handle: params[:username])
-    # @user.fetch_tweets!
-    erb :load_tweets
-  elsif @user.tweets_stale?   # AJAX request should refresh the cache,
-                              # returning list of tweets to be rendered
-    @user.fetch_tweets! 
-
-    erb :load_tweets 
-  else  
-    @tweets = @user.tweets.limit(10)
-    @tweets.reverse!
-    erb :show_tweets
+post '/tweet' do
+  begin
+    # p params
+    sleep 2
+    #getting params[:tweet] from index.erb
+    Twitter.update(params[:tweet])
+    @message = "Your tweet was sent successfully!"
+    erb :index
+  rescue
+    @error = "Something went wrong, sorry!  You know you can't tweet the same \
+    thing twice, right?"
+    erb :index
   end
 end
 
-post '/find_user' do
 
-  redirect to "/#{params[:handle]}"
-end
+# get '/:username' do
+#   @user = TwitterUser.find_by_handle(params[:username])
+
+#   if !@user 
+
+#     @user = TwitterUser.create(handle: params[:username])
+#     # @user.fetch_tweets!
+#     erb :load_tweets
+#   elsif @user.tweets_stale?   # AJAX request should refresh the cache,
+#                               # returning list of tweets to be rendered
+#     @user.fetch_tweets! 
+
+#     erb :load_tweets 
+#   else  
+#     @tweets = @user.tweets.limit(10)
+#     @tweets.reverse!
+#     erb :show_tweets
+#   end
+# end
+
+# post '/find_user' do
+
+#   redirect to "/#{params[:handle]}"
+# end
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
-  $('div#ajaxify').prepend('<img src="ajax-loader.gif />'); 
-   $('div#ajaxify').hide();
+  // $('div#ajaxify').prepend('<img src="ajax-loader.gif />'); 
+  //  $('div#ajaxify').hide();
+
+  $('form').submit(function(event){
+    event.preventDefault();
+    $('p.error').hide();
+    var tweet = {};
+    tweet.tweet = $('.tweet-input').val();
+
+    // disable the form
+    $('.tweet-input').prop('disabled', true);
+    $('.tweet-button').prop('disabled', true);
+
+    // append a message
+    $('form').append('<p class="wait">You tweet is being sent!</p>');
+
+    $.ajax({
+      type: "POST",
+      url: '/tweet',
+      data: tweet,
+      success: function(success){
+        $('body').html(success);
+      }
+    });
+  });
 
 //@user.fetch_tweets!
   // This is called after the document has loaded in its entirety
